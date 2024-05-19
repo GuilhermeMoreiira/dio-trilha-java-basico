2228ce7d2de9030ff848d8add7d8ef639def5e9d
Guilherme Moreira <159220309+GuilhermeMoreiira@users.noreply.github.com>
2024-05-19 11:10:02 -0300

    Finalizando o projeto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,9 +3,11 @@ import java.util.Locale;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+
+        // Criando um novo scanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        float saldo = 2.500f;
+        float saldo = 237.48f;
 
         System.out.println("Olá, qual é seu nome?");
         String nome = scanner.next();
@@ -17,7 +19,7 @@ public class ContaTerminal {
         int conta = scanner.nextInt();
 
         System.out.println(
-            "Olá " + nome + "," + " obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + " conta: " + conta + " e o seu saldo de " + saldo + " já está disponivel para saque!"
+            "Olá " + nome + "," + " obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + " conta: " + conta + " e o seu saldo de " + "R$" + saldo + " já está disponivel para saque!"
         );
     }
 }
