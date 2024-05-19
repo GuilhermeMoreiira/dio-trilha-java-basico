5ab78b131d2902737783d269d85ca7dbff6c1402
Guilherme Moreira <159220309+GuilhermeMoreiira@users.noreply.github.com>
2024-05-19 11:00:20 -0300

    Inicializando o exercício

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
